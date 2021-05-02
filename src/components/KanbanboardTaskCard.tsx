b825a4cef5d9cd96e97d31edd3829208a7db4337
ga0hyeon <idoll0513@gmail.com>
2021-05-02 23:13:45 +0900

    added task modify modal

--- src/components/KanbanboardTaskCard.tsx
+++ src/components/KanbanboardTaskCard.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './KanbanboardTaskCard.scss'
 import doubleArrowDown from '../resource/icons/double_arrow_down.svg'
 import doubleArrowUp from '../resource/icons/double_arrow_up.svg'
 import arrowDown from '../resource/icons/arrow_down.svg'
 import arrowUp from '../resource/icons/arrow_up.svg'
 import dash from '../resource/icons/single_dash.svg'
-import { Task } from '../data'
+import { Sprint, sprintState, Step, Task } from '../data'
+import KanbanboardTaskModifyModal from './KanbanboardTaskModifyModal'
+import { useRecoilState } from 'recoil'
 const KanbanboardTaskCard: React.FC<Task> = (props: Task) => {
+    const [sprint, setSprint] = useRecoilState(sprintState)
+    const [showModifyModal, setShowModifyModal] = useState<boolean>(false)
     const priorityIconList = [
         doubleArrowDown,
         arrowDown,
@@ -18,22 +22,65 @@ const KanbanboardTaskCard: React.FC<Task> = (props: Task) => {
         e.dataTransfer.setData('prevStepId', props.stepId)
         e.dataTransfer.setData('taskId', props.taskId)
     }
+    const handleDoubleClick = (e: any) => {
+        e.preventDefault()
+        if (!showModifyModal) setShowModifyModal(true)
+    }
+    const handleModifyModalHide = () => {
+        setShowModifyModal(false)
+    }
+    const handleModifyModalSave = (newTask: Task) => {
+        setSprint((oldSprint: Sprint) => {
+            const stepList: Step[] = JSON.parse(
+                JSON.stringify(oldSprint.stepList)
+            )
+
+            const taskIndex = stepList[newTask.stepId].taskList.findIndex(
+                (task) => {
+                    return task.taskId === newTask.taskId
+                }
+            )
+
+            if (taskIndex > -1) {
+                //exchange old task to new task
+                stepList[newTask.stepId].taskList[taskIndex] = newTask
+
+                //reorder task list
+                stepList[newTask.stepId].taskList = stepList[
+                    newTask.stepId
+                ].taskList.sort((a: Task, b: Task) => {
+                    return b.priority - a.priority
+                })
+            }
+
+            return { ...oldSprint, stepList: stepList }
+        })
+    }
     return (
-        <div
-            id="KanbanboardTaskCard"
-            draggable={true}
-            onDragStart={handleDragStart}
-        >
-            <div className="task-title">{props.taskTitle}</div>
-            <div className="task-desc">{props.taskDesc}</div>
-            <div className="task-priority">
-                <img
-                    src={priorityIconList[props.priority]}
-                    alt={`task_priority_${props.priority}`}
-                />
+        <>
+            <div
+                id="KanbanboardTaskCard"
+                draggable={true}
+                onDragStart={handleDragStart}
+                onDoubleClick={handleDoubleClick}
+            >
+                <div className="task-title">{props.taskTitle}</div>
+                <div className="task-desc">{props.taskDesc}</div>
+                <div className="task-priority">
+                    <img
+                        src={priorityIconList[props.priority]}
+                        alt={`task_priority_${props.priority}`}
+                    />
+                </div>
+                <div className="task-difficulty">{props.diffuculty}</div>
             </div>
-            <div className="task-difficulty">{props.diffuculty}</div>
-        </div>
+            <KanbanboardTaskModifyModal
+                task={props}
+                show={showModifyModal}
+                onHide={handleModifyModalHide}
+                onSave={handleModifyModalSave}
+            />
+        </>
     )
 }
 
